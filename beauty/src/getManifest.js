b37f17677534ddbe2d1b835a5f26147dc2f2c3cf
Alexandr <voronovicha05@gmail.com>
2025-04-27 18:06:27 +0300

    dd

--- beauty/src/getManifest.js
+++ beauty/src/getManifest.js
@@ -3,11 +3,17 @@ const fs = require("fs");
 const { getIcons } = require("./getIcons.js");
 
 const getManifest = () => {
-    const config = YAML.parse(fs.readFileSync('beauty.config.yaml', 'utf8'));
+    const
+        config = YAML.parse(fs.readFileSync('beauty.config.yaml', 'utf8')),
+        shortcutsConfig = config["shortcuts"];
 
     if (config["icons"]) {
         const iconsConfig = config["icons"];
         config["general"].icons = getIcons( iconsConfig["icon"], iconsConfig["icon_dir"], iconsConfig["icons_path"]);
+    }
+
+    if (shortcutsConfig) {
+        config["general"].shortcuts = shortcutsConfig;
     }
 
     return config["general"];
